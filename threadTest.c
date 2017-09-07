5ceda227613e5e9db02f341a991b5806c49c86a4
jauthier <jauthier@mail.uoguelph.ca>
2017-09-07 13:47:47 -0400

    add test

---
+++ threadTest.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthreads.h>
+
+void * runner(void * args);
+
+int main(int argc, char const *argv[])
+{
+	
+	char Name[5] = "Jess";
+	//thread id
+	pthread_t thread;
+	//create attributes
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+
+	//create a pthread
+	pthread_create(&thread, &attr, runner, &name);
+
+	pthread_join(thread, NULL);
+	printf("done\n");
+	return 0;
+}
+
+void * runner(void * args){
+
+	char * name = (char *)args;
+	pthread_exit(0);
+}
+
