9e0271b6f322a0290899cc4dcc79771486a796a3
jauthier <jauthier@mail.uoguelph.ca>
2017-10-26 13:55:13 -0400

    encryption

--- encrypt_test.c
+++ encrypt_test.c
@@ -22,8 +22,7 @@ char * encrypt(char * input){
 	}
 	char * ret = malloc(sizeof(char)*(strlen(inDict)+1));
 	strcpy(ret, inDict);
-	char * eDict = letterScramble(ret);
-	return ret;
+	return inDict;
 }
 
 char * letterScramble(char * input){
@@ -35,20 +34,45 @@ char * letterScramble(char * input){
 
 	for (int i = 0;i<len;i++){
 		int swap = rand()%len;
-		printf("%d\n", swap);
 
 		char hold = eDict[i];
 		eDict[i] = eDict[swap];
 		eDict[swap] = hold;
 	}
-	printf("%s\n", eDict);
 	return eDict;
+}
+
+int getPos(char c, char * str){
+	int len = strlen(str);
+	for (int i=0;i<len;i++){
+		if (str[i] == c)
+			return i;
+	}
+	return -1;
 }
 
 int main(int argc, char const *argv[]){
 	char message[100] = "the cat";
-	char * ret = encrypt(message);
-	printf("%s\n", ret);
-	free(ret);
+	char * inDict = encrypt(message);
+	printf("%s\n", inDict);
+	char * eDict = letterScramble(inDict);
+	printf("%s\n", eDict);
+
+	int len = strlen(message);
+	char * enMsg = malloc(sizeof(char)*(len+1));
+	for (int i=0;i<len;i++){
+		if (message[i] != ' '||message[i] != '\n'){
+			int pos = getPos(message[i], inDict);
+			enMsg[i] = eDict[pos];
+		} else {
+			enMsg[i] = message[i];
+		}
+	}
+	enMsg[len] = '\0';
+	printf("%s\n", enMsg);
+
+	free(inDict);
+	free(eDict);
+	free(enMsg);
 	return 0;
 }
