9549cd970030ed8cdf909105832f6df4c325db98
jauthier <jauthier@mail.uoguelph.ca>
2017-10-28 13:21:50 -0400

    encryption

--- encrypt_test.c
+++ encrypt_test.c
@@ -149,16 +149,16 @@ int main(int argc, char const *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
 	if (myRank != 0){
-		MPI_Recv(msg,26,MPI_CHAR,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		//MPI_Recv(msg,26,MPI_CHAR,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 		// get all possible combinations
 		//generate(numMPI, msg, myRank);
-		MPI_Send(msg, strlen(msg)+1, MPI_CHAR,0,0,MPI_COMM_WORLD);
+		MPI_Send(strs[myRank], strlen(strs[myRank])+1, MPI_CHAR,0,0,MPI_COMM_WORLD);
 
 	} else {
 		// need to send the other processes their strings
 		for(int k=1;k<numMPI;k++){
 			printf("%s\n", strs[0]);
-			MPI_Send(strs[k], strlen(strs[k])+1, MPI_CHAR,k,0,MPI_COMM_WORLD);
+			//MPI_Send(strs[k], strlen(strs[k])+1, MPI_CHAR,k,0,MPI_COMM_WORLD);
 			//generate(numMPI, strs[0],0);
 			MPI_Recv(msg,26,MPI_CHAR,k,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 			printf("%s\n", msg);
