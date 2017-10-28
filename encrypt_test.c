1352efe7d71d3040dc1212326688413b25c31e42
jauthier <jauthier@mail.uoguelph.ca>
2017-10-28 13:44:49 -0400

    encryption

--- encrypt_test.c
+++ encrypt_test.c
@@ -81,7 +81,7 @@ void generate(int n, char * str, int rank, char * inDict){
 	if (n == 1){
 		if (rank != 0){
 			if (strcmp(str, inDict)==0)
-				MPI_Send(strs[myRank], strlen(strs[myRank])+1, MPI_CHAR,0,1,MPI_COMM_WORLD);
+				MPI_Send(str, strlen(str)+1, MPI_CHAR,0,1,MPI_COMM_WORLD);
 		} else {
 			if (strcmp(str, inDict)==0)			
 				printf("%s\n", str);
