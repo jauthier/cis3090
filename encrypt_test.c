fcfc2b0e797e86e7c666724c5c99f6290c2e5709
jauthier <jauthier@mail.uoguelph.ca>
2017-10-28 17:08:52 -0400

    encryption

--- encrypt_test.c
+++ encrypt_test.c
@@ -86,8 +86,7 @@ char * generate(int n, char * str, char * inDict, char * message){
 		char * ret = generate(n-1, str, inDict, message);
 		if (ret != NULL){
 			char * unEnMsg = decryption(message,inDict,ret);
-			printf("%s\n", unEnMsg);
-			/*char * token = strtok(unEnMsg, " ");
+			char * token = strtok(unEnMsg, " ");
 			int words = 1;
 			while (token != NULL){
 				char * cmd;
@@ -96,8 +95,8 @@ char * generate(int n, char * str, char * inDict, char * message){
 				if (check != 0)
 					words = 0;
 				token = strtok(NULL, " ");
-			}*/
-			if (strcmp(unEnMsg, "the cat") == 0)
+			}
+			if (words == 1)
 				return ret;
 		}
 		
