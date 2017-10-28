eeb27bb9b2cc06fef26d99e893430cdde5989d19
jauthier <jauthier@mail.uoguelph.ca>
2017-10-28 17:21:09 -0400

    encryption

--- encrypt_test.c
+++ encrypt_test.c
@@ -110,7 +110,7 @@ char * generate(int n, char * str, char * inDict, char * message){
 
 int main(int argc, char const *argv[]){
 	
-	char message[100] = "the cat says hello";
+	char message[100] = "the wolf";
 	/* starting MPI stuff */
 	int myRank;
 	int numMPI;
