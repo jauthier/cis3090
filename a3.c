1e5ed6dd9f45512053e4998dd0881f13af31b36a
jauthier <jauthier@mail.uoguelph.ca>
2017-11-07 12:18:10 -0500

    a3

---
+++ a3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int ** initMatrix(int size){
+	srand(time(NULL));
+	int ** matrix = malloc(sizeof(int *)*size);
+	int i = 0;
+	for (i=0;i<size,i++){
+		matrix[i] = malloc(sizeof(int)*size);
+		int j = 0;
+		for (j=0;j<size;j++){
+			//get random num
+			int num = rand()%21;
+			matrix[i][j] = num;
+		}
+	}
+	return matrix;
+}
+
+int * initVector(int size){
+
+}
+
+void printMAtrix(int ** matrix, int size){
+	int i = 0;
+	for (i=0;i<size,i++){
+		int j = 0;
+		for (j=0;j<size;j++){
+			printf("%d", matrix[i][j]);
+		}
+		printf("\n");
+	}	
+}
+
+
+int main(int argc, char * argv[]){
+	
+	if (argc < 1){
+		printf("Please provide the number of threads and the size of the the matrix.\n");
+		exit(0);
+	}
+
+
+
+}
